c6e4bf6670b5ff5a5053f374f6bf6edf97754855
Luca Auer <lolle2000.la@gmail.com>
2020-03-24 18:52:28 +0100

    Add command for help link

---
+++ src/AdonisUI/Controls/HelpLinkCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AdonisUI.Controls
+{
+    class HelpLinkCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private readonly Action callback;
+
+        public HelpLinkCommand(Action callback)
+        {
+            this.callback = callback;
+        }
+
+        public bool CanExecute(object parameter) => callback != null;
+
+        public void Execute(object parameter) => callback?.Invoke();
+    }
+}
