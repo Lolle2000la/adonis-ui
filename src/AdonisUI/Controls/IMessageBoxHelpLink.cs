f627f3e215c7f13ebc3c04fa0e2bce3095ccf0c5
Luca Auer <lolle2000.la@gmail.com>
2020-03-24 15:45:12 +0100

    Specify contract

--- src/AdonisUI/Controls/IMessageBoxHelpLink.cs
+++ src/AdonisUI/Controls/IMessageBoxHelpLink.cs
@@ -1,9 +1,21 @@
-﻿namespace AdonisUI.Controls
+﻿using System;
+
+namespace AdonisUI.Controls
 {
     /// <summary>
     /// Exposes properties used to configure the appearance and behavior of a help link on a <see cref="MessageBoxWindow"/>.
     /// </summary>
     public interface IMessageBoxHelpLink
     {
+        /// <summary>
+        /// A <see cref="String"/> that specifies which caption to use for the help link.
+        /// </summary>
+        string Caption { get; }
+
+        /// <summary>
+        /// A <see cref="Action"/> callback that will be called when the link is pressed.
+        /// </summary>
+        /// 
+        Action Callback { get; }
     }
 }
