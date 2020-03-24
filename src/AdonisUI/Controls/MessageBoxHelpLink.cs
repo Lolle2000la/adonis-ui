018849dc71409fc742d58fac1d09cee59ee6bc7c
Luca Auer <lolle2000.la@gmail.com>
2020-03-24 15:52:08 +0100

    Fulfill IMessageBoxHelpLink contract

---
+++ src/AdonisUI/Controls/MessageBoxHelpLink.cs
@@ -0,0 +1,34 @@
+﻿using AdonisUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdonisUI.Controls
+{
+    /// <inheritdoc/>
+    public class MessageBoxHelpLink
+        : PropertyChangedBase
+        , IMessageBoxHelpLink
+
+    {
+        private string _caption = "Learn more";
+
+        /// <inheritdoc/>
+        public string Caption
+        {
+            get => _caption;
+            private set => SetProperty(ref _caption, value);
+        }
+
+        private Action _callback;
+
+        /// <inheritdoc/>
+        public Action Callback
+        {
+            get => _callback;
+            private set => SetProperty(ref _callback, value);
+        }
+    }
+}
